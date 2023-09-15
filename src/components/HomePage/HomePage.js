39b606b9bedba304f422d589c272bfea0b731f4e
Radhika Joshi <radhika7002@gmail.com>
2023-09-15 15:51:43 -0700

    Update Resume

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import Button from "react-bootstrap/Button";
 import "./HomePage.css";
-import Resume from "../../assets/RadhikaJoshiResume.pdf";
+import Resume from "../../assets/RadhikaJoshi_Resume.pdf";
 import AboutPage from "../AboutPage/AboutPage";
 import ExperiencePage from "../ExperiencePage/ExperiencePage";
 import PortfolioPage from "../PortfolioPage/PortfolioPage";
